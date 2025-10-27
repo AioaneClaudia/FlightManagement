a8c0b0d91bdc16995505ce6aa1e9da56b2bd4105
andreea1306 <andreealdea13@gmail.com>
2025-10-27 22:33:54 +0200

    Repository

--- src/main/java/com/example/flightmanagement/DemoApplication.java
+++ src/main/java/com/example/flightmanagement/DemoApplication.java
@@ -1,1 +1,15 @@
+package com.example.flightmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Flight Management System lauft!");
+        System.out.println("Testează aplicația la: http://localhost:8080/hello");
+    }
+}
 
